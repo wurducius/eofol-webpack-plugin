8185675e3521a78bafa41b22bcc92c600546f6e3
wurducius <wurducius@gmail.com>
2025-08-13 19:20:50 +0200

    Use better library for generating favicon and increment version

--- src/lib/generate-favicon.js
+++ src/lib/generate-favicon.js
@@ -1,11 +1,10 @@
 const { addAsset } = require("../util")
-const toIco = require("to-ico")
+const pack = require("ico-packer")
 
 const generateFavicon = (compilation) => {
   const iconContent = compilation.assets["assets/media/images/favicon.png"].source()
-  return toIco([iconContent]).then((buffer) => {
-    addAsset(compilation, "favicon.ico", buffer, {}, false)
-  })
+  const faviconContent = pack([iconContent])
+  addAsset(compilation, "favicon.ico", faviconContent, {}, false)
 }
 
 module.exports = generateFavicon
